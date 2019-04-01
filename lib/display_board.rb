1358cb321c51d71c777e9f16e9e6924b5ab2ad38
Oana Berentan <oana.berentan@yahoo.com>
2019-04-01 21:41:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
-def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     puts "   |   |   "
     puts "-----------"
-    puts "   |   |   "
+    puts "   | X |   "
     puts "-----------"
     puts "   |   |   "
     puts "X#{board[4]}"
